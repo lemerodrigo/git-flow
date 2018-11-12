4dfc5da24f678e3b808abe7a2df18b18643007c3
Andre Amancio <amancioandre@gmail.com>
2018-11-12 16:58:33 -0200

    tentativa

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 function makeBigorna() {
 	console.log('Saindo uma bigorna');
 	console.log('Editado na develop');
-<<<<<<< HEAD
-	for(let i=0; i < 100; i++) {
-		console.log('Tentei fazer a escada!');
-	}
-=======
-<<<<<<< HEAD
 	console.log('Gui!')
-=======
 	console.log('YOLO');
 	console.log('Andre')
->>>>>>> a31c630e530cb9d7c6ca76ea6c308325fda8fe9b
->>>>>>> 4600a06dc2d64baf564e150423b2f2c144a0ed2a
 }
