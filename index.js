e1be1d3fbd1578c59f6c22729fd83cefef7bc5c6
Mohsan <mohsanabbas>
2018-11-12 17:09:53 -0200

    hogya

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 function makeBigorna() {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   console.log('Mohsan Abbas');
   console.log('Shukriya Dosto');
-=======
-	console.log('Saindo uma bigorna');
-=======
-	console.log('Saindo duas bigorna');
->>>>>>> 5855d108108e51778869e8ac59f4e1af6f9f1511
-	console.log('Editado na develop');
-	console.log('Gui!')
-	console.log('YOLO');
-	console.log('Andre')
 }
-
-console.log('Modified by Ronaldo');
